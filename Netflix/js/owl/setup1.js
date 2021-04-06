c7329d5be5088388c8610173fe6e7e3f2b09abee
mplbruno <mplbruno@gmail.com>
2021-04-06 16:39:51 -0300

    Recriando a Interface Netflix Entregar projeto

---
+++ Netflix/js/owl/setup1.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+   
+       
+        loop:true,
+        margin:10,
+        nav:false,
+        video:true,
+      
+       
+        responsive:{
+            480:{
+                items:2
+            },
+            600:{
+                items:4
+        }
+    }
+})
---
+++ Netflix/js/setup2.js
@@ -0,0 +1,25 @@
+var carousel = $('.owl-carousel').owlCarousel({
+       
+    items:1,
+    loop:1,
+    nav:1,
+    dots:1
+})
+
+ $('.owl-carousel').owlCarousel({
+    items:1,
+    merge:true,
+    loop:true,
+    margin:10,
+    video:true,
+    lazyLoad:true,
+    center:true,
+    responsive:{
+        480:{
+            items:2
+},
+        600:{
+                items:4
+            }
+        }
+    })
